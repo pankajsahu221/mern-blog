01a067d27b8ccc9900b24b0ca17ffd82a6c9da72
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2022-07-24 22:52:13 +0530

    write page component created

--- client/src/pages/write/Write.js
+++ client/src/pages/write/Write.js
@@ -4,14 +4,32 @@ import "./Write.css";
 const Write = () => {
   return (
     <div className="write">
+      <img
+        className="writeImg"
+        src="https://cdn.britannica.com/49/179449-138-9F4EC401/Overview-Berlin.jpg?w=800&h=450&c=crop"
+        alt=""
+      />
       <form className="writeForm">
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
-            <i className="fas fa-plus"></i>
+            <i className="writeIcon fas fa-plus"></i>
           </label>
-          <input type="file" id="fileInput" />
-          <input type="text" placeholder="Title" />
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
         </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
       </form>
     </div>
   );
