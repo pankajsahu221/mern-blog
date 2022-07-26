3e7574c797f42ac8fb10c3c9f61b36ceb3868631
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2022-07-26 08:59:21 +0530

    login and register page created

---
+++ client/src/pages/login/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label>Email</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter your email"
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="Enter your password"
+        />
+        <button className="loginButton">Login</button>
+      </form>
+      <button className="loginRegisterButton">Register</button>
+    </div>
+  );
+};
+
+export default Login;
